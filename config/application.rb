9248f4467c6dfe4bf5818754db6638638d3366c4
Oliver Mettke <rookie81@gmx.net>
2011-05-21 01:52:03 +0200

    Korrektur Signin

--- config/application.rb
+++ config/application.rb
@@ -38,6 +38,6 @@ module SampleApp
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password, :secret_code]
+    config.filter_parameters += [:password]
   end
 end
